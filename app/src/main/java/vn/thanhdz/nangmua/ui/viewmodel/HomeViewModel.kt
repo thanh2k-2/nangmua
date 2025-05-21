3ca892058ff5c12b69a5264c2bbd81557a97f142
thanh2k-2 <88234312+thanh2k-2@users.noreply.github.com>
2025-05-21 18:27:55 +0700

    Update HomeViewModel.kt

--- app/src/main/java/vn/thanhdz/nangmua/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/vn/thanhdz/nangmua/ui/viewmodel/HomeViewModel.kt
@@ -1,25 +1,23 @@
 package vn.thanhdz.nangmua.ui.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import vn.thanhdz.nangmua.data.model.CurrentWeather
-import vn.thanhdz.nangmua.data.model.CitySearch
+import vn.thanhdz.nangmua.data.model.Cur
 import vn.thanhdz.nangmua.data.repository.WeatherRepository
 import vn.thanhdz.nangmua.data.repository.WeatherRepositoryImp
 import vn.thanhdz.nangmua.data.resource.Results
 
 class HomeViewModel(
-    private var weatherRepository: WeatherRepository = WeatherRepositoryImp()
-) : ViewModel() {
+    private var weatherRepository: WeatherRepository = WeatherRepositoryImp(),
+
+    ) : ViewModel() {
     private val _currentWeather: MutableLiveData<CurrentWeather> = MutableLiveData()
     val currentWeather: LiveData<CurrentWeather> = _currentWeather
-    private val _isLoad: MutableLiveData<Boolean> = MutableLiveData()
-    val isLoad: LiveData<Boolean> = _isLoad
-    private val _citySearch: MutableLiveData<CitySearch> = MutableLiveData()
-    val citySearch: LiveData<CitySearch> = _citySearch
 
 
     fun loadDataCurrentWeather(locationId: Int) {
@@ -27,12 +25,11 @@ class HomeViewModel(
             val result = weatherRepository.loadCurrentWeather(locationId)
             if (result is Results.Success) {
                 _currentWeather.postValue(result.data.firstOrNull())
-                _isLoad.postValue(true)
             } else {
                 _currentWeather.postValue(null)
-                _isLoad.postValue(false)
             }
         }
     }
+
 
 }
